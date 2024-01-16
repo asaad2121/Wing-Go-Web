6014ae42d0a5a95c371fadc75acfe1d1f85337fe
Mohmmed Asaad Shaikh <mohmmed.shaikh@remitbee.com>
2024-01-16 23:02:36 +0530

    GraphQL Client Added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
 import { AppProps } from 'next/app';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ReduxProvider } from '@/redux/provider';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql',
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <>
       <ReduxProvider>
         <CssBaseline/>
-        <Component {...pageProps} />
+          <ApolloProvider client={client}>
+            <Component {...pageProps} />
+          </ApolloProvider>
       </ReduxProvider>
   </>)
 }
