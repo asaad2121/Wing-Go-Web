a8b1b517d0dae05f5f0db61f1c8756cc19b8fb7b
M Asaad Shaikh <Mohmmed Asaad Shaikh>
2025-04-30 19:03:12 +1200

    Dashboard stuff added

--- src/pages/Dashboard/DashboardWizard.tsx
+++ src/pages/Dashboard/DashboardWizard.tsx
@@ -1,15 +1,44 @@
-import NavBar from '@/components/Navbar/NavBar';
-import { Box } from '@mui/material';
-import classes from './DashboardWizard.module.scss';
 import React from 'react';
+import Carousel from 'react-material-ui-carousel';
+import { Box, Paper, Typography } from '@mui/material';
+import classes from './DashboardWizard.module.scss';
+
+interface StepItem {
+    title: string;
+    description: string;
+}
 
 const DashboardWizard: React.FC = () => {
+    const steps: StepItem[] = [
+        {
+            title: 'Card 1',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+        },
+        {
+            title: 'Card 2',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        },
+        {
+            title: 'Card 3',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+        },
+    ];
+
     return (
         <>
-            <NavBar />
             <Box className={classes['wg-dashboard-container']}>
-                <h1>Dashboard Wizard</h1>
-                <p>Welcome to the Dashboard Wizard. Follow the steps to set up your dashboard.</p>
+                <div className={classes['wg-carousel-container']}>
+                    <Carousel animation="slide" navButtonsAlwaysVisible indicators autoPlay={false}>
+                        {steps?.map((step, index) => (
+                            <Paper key={index} className={classes['wg-carousel-slide']}>
+                                <Typography variant="h5" gutterBottom>
+                                    {step?.title}
+                                </Typography>
+                                <Typography variant="body1">{step?.description}</Typography>
+                            </Paper>
+                        ))}
+                    </Carousel>
+                </div>
             </Box>
         </>
     );
---
+++ src/shared/utility.ts
@@ -0,0 +1,22 @@
+/**
+ * @function updateObject
+ * @description It is used to update easily the objects instead of spread
+ * operators around all the code.
+ * @param {Object} actualState
+ * @param {Object} updatedValues
+ */
+export const updateObject = (actualState: object, updatedValues: object) => ({
+    ...actualState,
+    ...updatedValues,
+});
+
+export const getPageTitleFromPath = (path: string): string => {
+    if (path === '/') return 'Home';
+
+    const cleanPath = path.replace(/\[.*?\]/g, '');
+
+    const segments = cleanPath.split('/').filter(Boolean);
+    const words = segments.map((segment) => segment.replace(/-/g, ' ').replace(/\b\w/g, (c) => c.toUpperCase()));
+
+    return words.join(' ') || 'WingGo';
+};
